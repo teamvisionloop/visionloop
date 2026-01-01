529a2a0a5f6147a301f0651f3ef687c81d5dd2b0
teamvisionloop <teamvisionloop@gmail.com>
2026-01-01 22:41:01 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="py-8 md:py-12 px-4 md:px-6 lg:px-12 border-t border-border">
+    <footer className="py-8 md:py-12 px-4 md:px-6 lg:px-12">
       <div className="max-w-7xl mx-auto">
         <div className="flex flex-col md:flex-row justify-between items-center gap-4 md:gap-6">
          <img
